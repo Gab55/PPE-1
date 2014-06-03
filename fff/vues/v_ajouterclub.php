368579992328d655962cbb33efd3ed1695306ce4
Gab <gabrielgiroud@mail.com>
2014-06-03 10:18:59 +0200

    Retouche 2

--- fff/vues/v_ajouterclub.php
+++ fff/vues/v_ajouterclub.php
@@ -17,8 +17,8 @@
     
     <p>
     
-        <label for="nomdirigeant">Nom du directeur :</label>
-        <input id="nomdirigeant" type="text" name="nomDirecteur" value="" size="10" >
+        <label for="nomDirecteur">Nom du directeur :</label>
+        <input id="nomDirecteur" type="text" name="nomDirecteur" value="" size="10" >
     
     </p>
 
